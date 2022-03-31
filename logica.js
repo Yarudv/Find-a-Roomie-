acdb2e38e3f69fc5ee5a47130ed7d14b8f820dbf
Yarudv <102300461+Yarudv@users.noreply.github.com>
2022-03-31 16:54:06 -0300

    Version branch 2
    Implementacion de alerta con texto "bienvenido a find a roomie!" y comienzo de implementacion de ventana emergente para agregar logica con js y jquery

---
+++ logica.js
@@ -0,0 +1,8 @@
+window.alert("Bienvenidos a Find a Roomie!")
+alert("Bienvenido a Find a Roomie!")
+
+var blo = document.querySelector("#bloque .modal-container .ventana")
+function ventana(){
+      document.querySelector("#bloque .modal-container .ventana")
+}
+
