bf73b93ffd834419d33acd3f05fcd10e979065dc
Yarudv <102300461+Yarudv@users.noreply.github.com>
2022-04-01 18:44:16 -0300

    version 2.1
    Actualización de ventana popup con jquery

---
+++ funcionalidades/logica.js
@@ -0,0 +1,8 @@
+//  
+
+$(document).ready(function (){
+    function showcontainer(){
+        $('.show1').addClass('show');
+    }
+    setTimeout(showcontainer, 3000);
+});
--- logica.js
+++
@@ -1,8 +0,0 @@
-window.alert("Bienvenidos a Find a Roomie!")
-alert("Bienvenido a Find a Roomie!")
-
-var blo = document.querySelector("#bloque .modal-container .ventana")
-function ventana(){
-      document.querySelector("#bloque .modal-container .ventana")
-}
-
