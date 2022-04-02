236bbea624208d6f29183b4764430db70526457f
Yarudv <102300461+Yarudv@users.noreply.github.com>
2022-04-02 19:03:38 -0300

    Version2.2
    Se incluye botón de colores aleatorios en el apartado de ayuda

--- funcionalidades/logica.js
+++ funcionalidades/logica.js
@@ -1,8 +1,31 @@
-//  
+window.alert("Bienvenidos a Find a Roomie!")
 
 $(document).ready(function (){
-    function showcontainer(){
-        $('.show1').addClass('show');
-    }
-    setTimeout(showcontainer, 3000);
+    setTimeout(function(){
+        $('.show').css('display', 'none')
+    }, 6000);
 });
+
+// js para boton de colores aleatorios de fondo
+function generarNuevoColor(){
+	var simbolos, color;
+	simbolos = "0123456789ABCDEF";
+	color = "#";
+
+	for(var i = 0; i < 6; i++){
+		color = color + simbolos[Math.floor(Math.random() * 16)];
+	}
+
+	document.body.style.background = color;
+	document.getElementById("hexadecimal").innerHTML = color;
+	document.getElementById("text").innerHTML = "Copiar Color";
+}
+function copiarAlPortapapeles(hexadecimal) {
+	var aux = document.createElement("input");
+	aux.setAttribute("value", document.getElementById(hexadecimal).innerHTML);
+	document.body.appendChild(aux);
+	aux.select();
+	document.execCommand("copy");
+	document.body.removeChild(aux);
+	document.getElementById("text").innerHTML = "Copiado!";
+}
